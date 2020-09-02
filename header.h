339991e9672aec75a229edec3ce1435d529cbd84
Lule93 <61776418+Lule93@users.noreply.github.com>
2020-09-02 16:48:15 -0500

    Create header.h

---
+++ header.h
@@ -0,0 +1,4 @@
+Using text editor, Emacs. 
+I created a header file "header.h" 
+I created main.cc , I need to include header.h in to main.cc and be able to test it to make sure its included.
+I keep getting errors. 
